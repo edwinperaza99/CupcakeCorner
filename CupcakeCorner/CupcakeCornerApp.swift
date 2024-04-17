3c9e07c2c27c56e913a84fee56eff6dcd233bc55
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-17 00:57:50 -0700

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by csuftitan on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
