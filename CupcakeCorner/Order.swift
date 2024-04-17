1a3a5a0ab82a885a9da0ada7c9d415e13cb55db3
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-17 12:46:16 -0700

    Create cupcake model

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Edwin on 4/17/24.
+//
+
+import SwiftUI
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    var type = 0
+    var quantity = 3
+
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
