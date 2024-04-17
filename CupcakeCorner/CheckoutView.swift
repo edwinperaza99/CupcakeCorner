f7820e83e86d92d6ce5683249b8f21fea59ffcf7
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-17 15:57:56 -0700

    create checkout view

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,52 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Edwin on 4/17/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+
+                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
