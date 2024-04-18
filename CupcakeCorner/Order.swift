9ff9940f4b90bb8e38b00b8f67d8467e0d4a9ae3
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-18 12:39:43 -0700

    add extra validation for address input

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -8,7 +8,19 @@
 import SwiftUI
 
 @Observable
-class Order {
+class Order: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
 
     var type = 0
@@ -31,11 +43,47 @@ class Order {
     var city = ""
     var zip = ""
     
+    var nameIsValid: Bool {
+        !name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+    
+    var streetAddressIsValid: Bool {
+        !streetAddress.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+    
+    var cityIsValid: Bool {
+        !city.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+    
+    var zipIsValid: Bool {
+        let trimmedZip = zip.trimmingCharacters(in: .whitespacesAndNewlines)
+        // Check if the zip is not empty and contains only numbers, 5 digits
+        return !trimmedZip.isEmpty &&
+               trimmedZip.allSatisfy { $0.isWholeNumber } &&
+               trimmedZip.count == 5
+    }
+    
     var hasValidAddress: Bool {
-        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
-            return false
+        nameIsValid && streetAddressIsValid && cityIsValid && zipIsValid
+    }
+    
+    var cost: Double {
+        // $2 per cake
+        var cost = Double(quantity) * 2
+
+        // complicated cakes cost more
+        cost += (Double(type) / 2)
+
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Double(quantity)
         }
 
-        return true
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Double(quantity) / 2
+        }
+
+        return cost
     }
 }
